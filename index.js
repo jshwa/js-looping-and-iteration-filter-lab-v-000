1ca382a400e55a213ef31253461827ac6089b7ef
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 21:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 function findMatching(drivers, name) {
   return drivers.filter(function(driver)  {return driver.toUpperCase() === name.toUpperCase()})
 }
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.slice(string.length) === string
+  })
+}
