9e59cc2d17fde6a466867929c0b24c195f5b32b2
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 21:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-  return drivers.filter(function(driver){
-    if driver.toUpperCase() === name.toUpperCase() {
-      return driver; })
-}
+  return drivers.filter(
+    function(driver)  {
+      if driver.toUpperCase() === name.toUpperCase() {
+        return driver; } })
 }
