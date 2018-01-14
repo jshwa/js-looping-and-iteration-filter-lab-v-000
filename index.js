b156b43373adea5742104eb50448567520b12449
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-14 21:45:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
-  return drivers.filter(function(driver)  {return driver.toUpperCase() === name.toUpperCase()})
+  return drivers.filter(function(driver)  {return driver.toUpperCase() === name.toUpperCase()});
 }
 
 function fuzzyMatch(drivers, string) {
   return drivers.filter(function (driver) {
-    return driver.slice(0, string.length) === string
+    return driver.slice(0, string.length) === string;
   })
 }
 
 function matchName(drivers, string) {
   return drivers.filter(function (driver) {
-    return driver.name === string
+    return driver.name === string;
   })
 }
